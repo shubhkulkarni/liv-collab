c601ab97c4f78c0859253c62bfcc7c9ba9e30025
Shubham Kulkarni <shubhamkulkarni.sh@gmail.com>
2021-05-16 21:22:10 +0530

    minor

--- src/pages/Confirm/Confirm.js
+++ src/pages/Confirm/Confirm.js
@@ -23,7 +23,7 @@ function Confirm() {
         class="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
         id="inline-full-name"
         type="text"
-        value="Jane Doe"
+        value="abd-hjuy-pqr"
       />
     );
   };
