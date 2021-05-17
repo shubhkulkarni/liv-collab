fdcb61dd3a31e1e03fcc74d081c57702ba1189eb
Shubham Kulkarni <shubhamkulkarni.sh@gmail.com>
2021-05-17 20:52:34 +0530

    prod setup

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,7 +1,7 @@
 import React from "react";
 import socketio from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:8000";
+const SOCKET_URL = "https://pure-woodland-56426.herokuapp.com/";
 
 export const socket = socketio.connect(SOCKET_URL);
 export const SocketContext = React.createContext();
